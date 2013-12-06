37c3e1be56feb607d0392204c6403c628d47669d
Thomas <nds.thomas@gmail.com>
2013-12-06 02:20:16 +0100

    matrix server

--- js/communication.js
+++ js/communication.js
@@ -2,7 +2,7 @@ var socket = null;
 var observations = {};
 var host = location.origin.replace(/^http/, 'ws')
 var ws = new WebSocket(host);
-
+var ID = Math.floor(Math.random() * 9999);
 /**
  * Constructeur de l'app client
  */
@@ -12,63 +12,9 @@ function init() {
         console.log(JSON.parse(event.data));
     };
 
-//    $('#gaucher').on('click', function() {
-
+    $('#play').on('click', function() {
+        console.log("play");
+        ws.send(JSON.stringify({"type": "ID","data": ID}));
+    });
 }
 
-function createMatrix(sx, sy) {
-    var arr = [];
-    for (i = 0; i < sx; i++) {
-        var row = {};
-        var subarr = [];
-        for (j = 0; j < sy; j++) {
-            cas = {"id": j, "lvl": 0, "type": "vide", "clock": 0, "img": ""};
-            subarr.push(cas);
-        }
-        row = {'columns': subarr, "row": i};
-        arr.push(row);
-    }
-    return {"map": arr};
-
-}
-gridgame = createMatrix(4,4);
-gridgame.map[i].columns[j].type;
-
-var truc = {
-    "map": [{
-            "row": "0",
-            "columns": [
-                {"id": "0", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "1", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "2", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "3", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"}
-            ]
-        },
-        {
-            "row": "1",
-            "columns": [
-                {"id": "0", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "1", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "2", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "3", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"}
-            ]
-        },
-        {
-            "row": "2",
-            "columns": [
-                {"id": "0", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "1", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "2", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "3", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"}
-            ]
-        },
-        {
-            "row": "3",
-            "columns": [
-                {"id": "0", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "1", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "2", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"},
-                {"id": "3", "lvl": "0", "type": "vide", "clock": "0", "img": "/img/truc.jpg"}
-            ]
-        }]
-};
--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var LC = 0; //clock virtuelle du jeu 
 var map = [[]];
 var timeRate = 1000;
+var gridmap = createMatrix(20,10);
 
-var map;
 var WebSocketServer = require('ws').Server
-  , http = require('http')
-  , express = require('express')
-  , app = express()
-  , port = process.env.PORT || 4242;
+        , http = require('http')
+        , express = require('express')
+        , app = express()
+        , port = process.env.PORT || 4242;
 
 app.use(express.static(__dirname + '/'));
 
@@ -21,8 +21,9 @@ console.log('websocket server created');
 
 wss.on('connection', function(ws) {
     var id = setInterval(function() {
-         LC += 1;
-        ws.send(JSON.stringify(map));
+        LC += 1;
+        ws.send(JSON.stringify(gridmap));
+        updateMap();
     }, timeRate);
 
     console.log('websocket connection open');
@@ -32,7 +33,42 @@ wss.on('connection', function(ws) {
         clearInterval(id);
     });
 
- ws.on('message', function(message) {
-        console.log('received: %s', message);
+    ws.on('message', function(message) {
+        var res = JSON.parse(message);
+        if (res.type = "ID") {
+            console.log("ID");
+            addSanta(res.data);
+        }
+        console.log('received: %s', res.data);
     });
 });
+
+function echanger (sx,sy,fx,fy){
+    var ech = gridmap.map[fx].columns[fy];
+    gridmap.map[fx].columns[fy] =  gridmap.map[fx].columns[sy];
+    gridmap.map[sx].columns[sy]=  ech;
+    
+}
+
+function addSanta(id) {
+    gridmap.map[0].columns[0] = {"id": id, "lvl": 1, "type": "santa", "clock": LC, "img": ""};
+}
+
+function createMatrix(sx, sy) {
+    var arr = [];
+    for (i = 0; i < sx; i++) {
+        var row = {};
+        var subarr = [];
+        for (j = 0; j < sy; j++) {
+            cas = {"id": j, "lvl": 0, "type": "vide", "clock": 0, "img": ""};
+            subarr.push(cas);
+        }
+        row = {'columns': subarr, "row": i};
+        arr.push(row);
+    }
+    return {"map": arr};
+}
+
+function updateMap(){
+    
+}
